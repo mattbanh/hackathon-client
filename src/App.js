820cb3de45e0bfb7bf7f55b8c0976f029b31d590
Matthew Banh <mattbanh92@gmail.com>
2022-12-02 11:42:21 -0800

    created pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import "./App.scss";
 
-import Hero from "./components/Hero/Hero";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import TopCarousel from "./components/TopCarousel/TopCarousel";
+import MainPage from "./pages/MainPage/MainPage";
+import LivePage from "./pages/LivePage/LivePage";
 
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
-      <TopCarousel />
-      {/* <BrowserRouter>
-         <Routes>
-          <Route path="/" element={<Marketing />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/404" element={<ErrorPage />} />
-          <Route path="*" element={<Navigate to="/404" />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/live" element={<LivePage />} />
+          {/* <Route path="/404" element={<ErrorPage />} /> */}
+          {/* <Route path="*" element={<Navigate to="/404" />} /> */}
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
       <Footer />
     </div>
   );
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import Hero from "../../components/Hero/Hero";
+import TopCarousel from "../../components/TopCarousel/TopCarousel";
+
+function MainPage() {
+  return (
+    <section className="main-page">
+      ;
+      <Hero />
+      <TopCarousel />
+    </section>
+  );
+}
+export default MainPage;
